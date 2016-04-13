36947df5c3155aeda1b4327832b0a29993287b07
GeraldCNelson <nelson.gerald.c@gmail.com>
2016-04-13 13:15:51 -0600

    Test of whether I have the github upload working.

--- R/nutrientModFunctions.R
+++ R/nutrientModFunctions.R
@@ -3,6 +3,7 @@
 #' @name nutrientModFunctions.R
 #' @author Gerald C. Nelson, \\email{nelson.gerald.c@@gmail.com}
 
+
 # .onLoad <- function(libname, pkgname) {
 #   op <- options()
 #   op.devtools <- list(
