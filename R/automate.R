b4d19791e283414f980bebb1cc978ebf745f1491
GeraldCNelson <nelson.gerald.c@gmail.com>
2016-08-04 09:54:33 -0600

    Added some descriptive printouts to the automate script.

--- R/automate.R
+++ R/automate.R
@@ -8,16 +8,24 @@
 
 # print(paste("start time is " , proc.time(), sep = ""))
 ptm <- proc.time()
+
+print("Running nutrientModFunctions.R")
 source("R/nutrientModFunctions.R")
+
+print("Running workbookFunctions.R")
 source("R/workbookFunctions.R")
+
+print("Running nutrientCalcFunctions.R")
 source("R/nutrientCalcFunctions.R")
 
+print("Running dataPrep.IMPACT.R")
 source("R/dataPrep.IMPACT.R")
 # - creates files in iData
 # dt.IMPACTmetaData
 # paste(dt,varName, sep = ".") - one file for each IMPACT variable, example is dt.PerCapKCAL.2016-06-21.rds
 # dt.CSEs
 
+print("Running dataPrep.SSP.R")
 source("R/dataPrep.SSP.R")
 # - creates files in mData
 # dt.SSPGDPClean - SSP GDP data
@@ -25,37 +33,45 @@ source("R/dataPrep.SSP.R")
 # dt.SSPPopClean - SSP population data including age and gender groups
 # dt.IMPACT159.pop.tot - total population by IMPACT 159 region from the SSP population data set; not any more
 
+print("Running dataPrep.regions.R")
 source("R/dataPrep.regions.R") # - creates dt.regions.all and the list of scenarios
 
 metadata()
-
+print("Running dataPrep.FBS.R")
 source("R/dataPrep.FBS.R") # - creates dt.FBS, mData
 
+print("Running dataManagement.fishnAlc.R")
 source("R/dataManagement.fishnAlc.R")
 # dt.fishIncElast, iData - to have a record of what fish income elasticities were used
 # dt.fishScenarios, mData
 # dt.alcIncElast, iData - to have a record of what alcohol income elasticities were used
 # dt.alcScenarios, mData - alcohol consumption scenarios, for input into dataManagement.IMPACT.R
 
+print("Running dataManagement.IMPACT.R")
 source("R/dataManagement.IMPACT.R")
 # adds fish and alcohol data, writes out IMPACT variables just for food items (names begin with c), and dt.IMPACTfood file
 #paste(fileShortName, "food, sep = "."), iData - just data for food commodities, example is dt.CSEs.food.2016-06-21.rds
 #dt.IMPACTfood, iData
 
+print("Running dataPrep.nutrientData.R")
 source("R/dataPrep.nutrientData.R") # - creates dt.cookingRet and dt.nutrients, mData. Note that
      # dt.nutrients does NOT take into account loss in cooking. That is done later and depends on a switch (search for switch.xxx .
 
+print("Running dataPrep.NutrientRequirements.R")
 source("R/dataPrep.NutrientRequirements.R")
 # newDFname, mData - nutrient requirements adjusted to SSP age and gender categories, example is req.RDA.macro.ssp.2016-06-22.rds
 
+print("Running dataManagement.SSPPop.R")
 source("R/dataManagement.SSPPop.R")
 #paste(gsub(".ssp","",nutReqName),"percap",sep = "."), mData - Nutrient requirements adjusted for population distribution, example is req.EAR.percap.2016-06-24.rds
 
+print("Running nutrientCalcs.R")
 source("R/nutrientCalcs.R")
 #writes
 # paste("food.agg.",reqShortName,sep = "") fileloc("resultsData"), "csv")
 # "dt.nutrients.sum", fileloc("resultsData"))
 
+print("Running nutCalcsProcessing.R")
 source("R/nutCalcsProcessing.R")
 #writes
 # paste(reqShortName, "all.sum", sep = ".")
@@ -68,6 +84,7 @@ source("R/nutCalcsProcessing.R")
 "all.req.ratio.cMin"
 # dt.energy.ratios - ratio of kcals from specific sources to total kcals
 
+print("Copying files for shiny app")
 source("R/copyFilestoNutrientModeling.R") # move results needed for the shiny app.R in the nutrientModeling folder
 
 proc.time() - ptm
